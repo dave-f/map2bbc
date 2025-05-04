c5b39fe1313eb1ee752bb91f70bf458f55f5d57c
dave-f <dave.footitt@gmail.com>
2025-05-04 19:48:18 +0100

    Add function to encode a mountain panic style level row

---
+++ panic.go
@@ -0,0 +1,88 @@
+package main
+
+import "errors"
+
+// Pack a row of screen bytes
+// It is expected the slice passed in has 8 bytes in it.
+//
+// The return slice is a variable length packed in the "Mountain Panic" format:
+// First byte is a control byte, with each bit set meaning a tile is present in that slot on the row.
+// Following this, either a run length byte:
+//                 0xf0 - Rest of the line is the same repeated tile, which follows this byte
+//                 0xfn - The next tile is repeated n times (n = 0-15)
+// Or a normal tile index:
+//                 0x00 - 0x1f
+
+func packLine(b []byte) ([]byte, error) {
+
+	if len(b) != 8 {
+
+		return nil, errors.New("bad slice")
+	}
+
+	var bitVal byte
+	r := make([]byte, 0, 9)
+
+	for b, i := range b {
+
+		if i != 0 {
+
+			bitVal |= 1 << (7 - b)
+		}
+	}
+
+	if bitVal == 0 {
+
+		r = append(r, 0) // Empty row, just return [0]
+		return r, nil
+	}
+
+	r = append(r, bitVal) // One bit set for each tile (i.e. 8 tiles in a row)
+
+	// Now do the RLE packing
+	idx := 0
+
+loop:
+	currentByte := b[idx]
+	rleCnt := 1
+
+	for idx < 7 && currentByte == b[idx+1] && currentByte != 0 {
+
+		rleCnt++
+		idx++
+	}
+
+	if rleCnt > 1 {
+
+		idx++
+	}
+
+	// If we have an RLE count of 1 or 2, just output the bytes
+	if rleCnt < 3 {
+
+		if currentByte != 0 {
+
+			for range rleCnt {
+
+				r = append(r, currentByte)
+			}
+		}
+
+		if rleCnt == 1 {
+
+			idx++
+		}
+	} else {
+
+		// output 0xf | rleCnt plus the byte itself
+		r = append(r, byte(0xf0|rleCnt))
+		r = append(r, currentByte)
+	}
+
+	if idx < 8 {
+
+		goto loop
+	}
+
+	return r, nil
+}
---
+++ panic_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestNilOrBadSlice(t *testing.T) {
+
+	_, err := packLine(nil)
+
+	if err == nil {
+
+		t.Errorf("Expected error")
+	}
+
+	_, err = packLine(make([]byte, 1))
+
+	if err == nil {
+
+		t.Errorf("Expected error")
+	}
+
+}
+
+func TestAllZeroSlice(t *testing.T) {
+
+	expected := make([]byte, 1)
+	got, err := packLine(make([]byte, 8))
+
+	if err != nil {
+
+		t.Errorf("Expected a nil error")
+	}
+
+	if slices.Compare(got, expected) != 0 {
+
+		t.Errorf("Unpexted return slice")
+	}
+}
+
+func TestRunLength(t *testing.T) {
+
+	tests := make([][]byte, 0)
+	expected := make([][]byte, 0)
+
+	tests = append(tests, []byte{0, 0, 0, 0, 0, 1, 1, 1})
+	expected = append(expected, []byte{0x07, 0xf3, 1})
+
+	tests = append(tests, []byte{8, 0, 0, 0, 0, 0, 0, 0})
+	expected = append(expected, []byte{0x80, 8})
+
+	tests = append(tests, []byte{0, 0, 0, 0, 0, 0, 0, 8})
+	expected = append(expected, []byte{0x01, 8})
+
+	tests = append(tests, []byte{1, 1, 1, 0, 0, 0, 0, 0})
+	expected = append(expected, []byte{0xe0, 0xf3, 1})
+
+	tests = append(tests, []byte{0, 0, 1, 1, 1, 0, 0, 0})
+	expected = append(expected, []byte{0x38, 0xf3, 1})
+
+	tests = append(tests, []byte{0, 0, 1, 1, 0, 0, 0, 0})
+	expected = append(expected, []byte{0x30, 1, 1})
+
+	tests = append(tests, []byte{0, 1, 1, 1, 0, 1, 1, 1})
+	expected = append(expected, []byte{0x77, 0xf3, 1, 0xf3, 1})
+
+	tests = append(tests, []byte{1, 1, 1, 1, 1, 1, 1, 1})
+	expected = append(expected, []byte{0xff, 0xf8, 1})
+
+	tests = append(tests, []byte{1, 1, 1, 1, 1, 0, 1, 1})
+	expected = append(expected, []byte{0xfb, 0xf5, 1, 1, 1})
+
+	for i, _ := range tests {
+
+		got, err := packLine(tests[i])
+
+		if err != nil {
+
+			t.Errorf("Unexpected error")
+		}
+
+		if slices.Compare(got, expected[i]) != 0 {
+
+			t.Errorf("Unexpected return slice")
+		}
+	}
+}
